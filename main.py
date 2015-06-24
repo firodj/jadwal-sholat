a3d80a9e24c9a958c8a4fd23140ed06ce20c9f46
Fadhil Mandaga <firodj@gmail.com>
2015-06-24 13:21:33 +0700

    add highlight and data.pyc

--- main.py
+++ main.py
@@ -1,25 +1,70 @@
-from bs4 import BeautifulSoup as Soup
-from bs4.element import Tag
-import time
+from datetime import datetime, timedelta
+from colorama import Fore, Style
 
-today = time.strftime('%Y%m%d')
+try:
+  from data import schedules
+except ImportError:
+  from bs4 import BeautifulSoup as Soup
+  from bs4.element import Tag
 
-soup = Soup(open('data.xml').read())
-schedules = {}
-for data in soup.findAll('data'):
-  schedule = {}
-  for tag in data.children:
-    if type(tag) != Tag:
+  f = open('data.py', 'w')
+  f.write("schedules = {\n")
+  soup = Soup(open('data.xml').read())
+  schedules = {}
+  for data in soup.findAll('data'):
+    schedule = {}
+    fields = []
+    for tag in data.children:
+      if type(tag) != Tag:
+        continue
+      schedule[tag.name] = tag.text
+      if not tag.name in ['year', 'month', 'date']:
+        fields.append(tag.name)
+    schedule_date = schedule.get('year','00') + schedule.get('month','00') + schedule.get('date','00')
+    schedules[schedule_date] = schedule
+
+    f.write('"' + schedule_date + '": {')
+    for field in fields:
+      f.write('"' + field + '": "' + schedule[field] + '", ')
+    f.write("},\n")
+  f.write("\n}\n")
+  f.close()
+
+def delta(today, hhmm):
+  praytime = datetime.strptime(today + ' ' + hhmm, '%Y%m%d %H:%M')
+  delta = praytime - datetime.now()
+  return delta
+
+today = datetime.now().strftime('%Y%m%d')
+schedule = schedules.get(today)
+
+if schedule:
+  rows = [('isha','Isya'),('maghrib','Maghrib'),('ashr','Ashar'),('dzuhr','Dzuhur'),('fajr','Subuh')]
+  nexts = {}
+
+  nearest = None
+  for row in rows:
+    nearnext = delta(today, schedule[row[0]])
+    nexts[row[0]] = nearnext
+
+    if nearnext.days < 0:
       continue
-    schedule[tag.name] = tag.text
-  schedule_date = schedule.get('year','00') + schedule.get('month','00') + schedule.get('date','00')
-  schedules[schedule_date] = schedule
+    if nearest is None:
+      nearest = row[0]
+      continue
+    if nexts[nearest] > nexts[row[0]]:
+      nearest = row[0]
 
-  if schedule_date == today:
-    print 'Jam:', time.strftime('%H:%I:%S');
-    print 'Tanggal:', schedule_date
-    print 'Magrib ', schedule['maghrib']
-    print 'Ashar  ', schedule['ashr']
-    print 'Dzhuhur', schedule['dzuhr']
-    print 'Shubuh ', schedule['fajr']
-    break
+  print 'Jam:', datetime.now().strftime('%H:%I:%S %Y-%m-%d');
+  for row in rows:
+    if nearest == row[0]:
+      print Fore.GREEN + Style.BRIGHT + row[1],
+    else:
+      print row[1],
+
+    print schedule[row[0]],
+
+    if nearest == row[0]:
+      print nexts[row[0]], Fore.RESET + Style.RESET_ALL
+    else:
+      print
